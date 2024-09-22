e8fdb09f7bc3e137fdfe2f7d059764b889a081ef
Pikachuuu <149296846+DADieAnotherDay@users.noreply.github.com>
2024-09-22 14:57:49 +0530

    Create install-sb.sh

---
+++ install-sb.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Selenium: Install via pip
+# Selenium is a collection of open-source tools that automate web browser testing
+#. It's used to mimic human interaction with a browser by replicating actions like clicking buttons and typing text. 
+# Selenium is primarily used for software testing, and can be run on many different browsers and operating systems. 
+# Selenium
+# What it does
+# Automates web browser testing by mimicking human interaction
+# How it works
+# Replicates actions like clicking buttons and typing text
+# What it's used for
+# Software testing
+# What it supports
+# Many browsers and operating systems
+# What it's made of
+# A collection of tools and libraries, including Selenium WebDriver, Selenium IDE, and Selenium Grid
+# Selenium is a member of the Software Freedom Conservancy, a 501(c)3 non-profit organization
+# The project's source code is freely available for anyone to use, improve, and enjoy. 
+
+pip3 install selenium
+
+# User Input: The user is prompted to enter file paths for the username and password lists.
+# Browser Automation: The script uses Selenium WebDriver to automate interactions with the Instagram login page.
+# Conclusion: The script exits once the correct password is found or all passwords are exhausted
+# Password Attempts: The script reads all passwords from the password file and attempts each one in sequence.
+
+python ethical_brouteFource.py
+
+
+
